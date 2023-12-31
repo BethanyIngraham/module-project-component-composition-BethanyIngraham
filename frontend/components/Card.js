22d32d259cf5a8084463f39d9afa6151dcaf69d5
Bethany Ingraham <BethFIngraham@gmail.com>
2023-12-31 01:56:35 -0500

    NASA photo update

--- frontend/components/Card.js
+++ frontend/components/Card.js
@@ -1,12 +1,38 @@
 import React from 'react'
 import Figure from './Figure'
+import styled from 'styled-components'
+
+const StyledCard = styled.div`
+  border: 1px solid black;
+  border-radius: 12px;
+  padding: 2rem;
+
+  h2 {
+    margin: 0 0 1rem 0;
+    font-size: 2.5em;
+    color: ${props => props.$color};
+    text-align: center;
+  }
+
+  p {
+    margin: 0 0 1.5rem 0;
+    color: steelblue;
+
+    &::first-line {
+      font-size: 1.5em;
+    }
+  }
+  
+`
+
+
 
 export default function Card({title, text, imageUrl, date}) {
     return (
-      <div className='card'>
+      <StyledCard $color='black' className='card'>
       <h2>{title}</h2>
       <p>{text}</p>
       <Figure imageUrl={imageUrl} caption={date}/>
-      </div>
+      </StyledCard>
     ) 
   }
--- frontend/components/Figure.js
+++ frontend/components/Figure.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const StyledFigure = styled.figure`
+  max-width: 60%;
+  
+  img {
+    border-style: solid;
+    border-color: black;
+    border-width: 5px;
+  }
+  
+  figcaption {
+    font-style: italic;
+  }
+`
 
 export default function Figure({imageUrl, caption}) {
     return (
-      <figure>
+      <StyledFigure>
       <img src={imageUrl}/>
       <figcaption>Photograph taken on {caption}</figcaption>
-    </figure>
+    </StyledFigure>
     )
   }
